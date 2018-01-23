739ca7873eb0f3452f1c29fed4da6f04e9742b64
User Name <pengfluf@tutanota.com>
2018-01-23 19:14:15 +0300

    API-request just works

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { WeatherService } from '../../services/weather.service';
+
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
+  weather: any;
 
-  constructor() { }
+  constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
+  }
+
+  getWeather(id: number): void {
+    if (id) {
+      console.log(`City id = ${id}`);
+      this.weatherService.getWeather(id)
+        .subscribe(weather => this.weather = weather;
+    }
   }
 
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { ListItem } from '../classes/ListItem';
+import { City } from '../classes/City';
 
 @Injectable()
 export class WeatherService {
   private api = {
-    url: 'http://api.openweathermap.org/data/2.5/forecast?id=524901',
+    url: 'http://api.openweathermap.org/data/2.5/forecast',
     key: 'b1d1a634025f6aad9fa10eaf9452b864',
   };
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getWeather(id: number): Observable<any> {
+    return this.http.get<any>(`${this.api.url}?id=${id}&APPID=${this.api.key}`);
+  }
 
 }
